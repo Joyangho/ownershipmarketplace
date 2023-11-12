ed1ae58f895e18c260680774f01894c6aab43b04
unknown <joborn2009@naver.com>
2023-11-12 19:28:24 +0900

    6

--- index.js
+++ index.js
@@ -81,6 +81,69 @@ function btnMintAmount(type) {
   }
 }
 
+// 사용자가 소유하고 있는 모든 토큰 ID를 가져오는 함수
+async function getOwnedTokens(walletAddress) {
+  // 사용자가 소유하고 있는 토큰의 수를 조회
+  let balance = await contract.methods.balanceOf(walletAddress).call();
+  let tokenIds = [];
+
+  // 각 토큰에 대해 토큰 ID를 조회
+  for (let i = 0; i < balance; i++) {
+      let tokenId = await contract.methods.tokenOfOwnerByIndex(walletAddress, i).call();
+      tokenIds.push(tokenId);
+  }
+
+  return tokenIds;
+}
+
+async function isOwner(walletAddress) {
+  // 사용자가 소유하고 있는 모든 토큰 ID를 가져옵니다.
+  let tokenIds = await getOwnedTokens(walletAddress);
+  
+  // 각 토큰에 대해 소유권을 체크합니다.
+  for (let tokenId of tokenIds) {
+      let ownerAddress = await contract.methods.ownerOf(tokenId).call();
+      if (ownerAddress === walletAddress) {
+          return true;
+      }
+  }
+
+  // 모든 토큰에 대해 소유권이 없는 경우
+  return false;
+}
+
+async function checkOwnership() {
+if (window.ethereum) {
+  await window.ethereum.send("eth_requestAccounts");
+  window.web3 = new Web3(window.ethereum);
+  contract = new web3.eth.Contract(ABI, ADDRESS);
+
+  if (isConnected) {
+    if (contract) {
+      // NFT 오너쉽 확인
+      const isNFTOwner = await isOwner(WalletAddress);
+      if (isNFTOwner) {
+        console.log('인증된 NFT 오너입니다.');
+        alert('인증된 NFT 오너입니다.');
+        window.location.href = 'ownership.html';
+      } else {
+        console.log('NFT 오너가 아닙니다.');
+        alert('NFT 오너가 아닙니다.');
+      }
+    }
+  } else {
+    connectWallet();
+  }
+} else {
+  alert("You need Metamask first!");
+}
+}
+
+// 버튼 클릭 시 checkOwnership 함수를 호출하도록 설정합니다.
+document.getElementById("checkOwnershipButton").addEventListener("click", function() {
+  checkOwnership();
+});
+
 async function mint() {
   if (window.ethereum) {
     await window.ethereum.send("eth_requestAccounts");
@@ -112,14 +175,5 @@ async function mint() {
     }
   } else {
     alert("You need Metamask first!");
-  }
-  const isOwner = await contract.methods.isOwner(txtWalletAddress).call();
-  if (isOwner) {
-    console.log('인증된 NFT 오너입니다.');
-    window.location.href = 'ownership.html';
-  } else {
-    console.log('NFT 오너가 아닙니다.');
-    // NFT 오너가 아닌 경우에 대한 처리를 추가하세요.
-    window.location.href = 'index.html'; // 예시로 에러 페이지로 이동하도록 설정했습니다.
   }
 }
